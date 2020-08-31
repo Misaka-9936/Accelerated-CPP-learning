da97a5750472dfedab7fe0957499e4d8f91c2988
Misaka 9936 <Misaka9936@gmail.com>
2020-09-01 00:38:47 +0800

    My learning notes and homeworks in Chapter Zero

--- Chapter Zero.cpp
+++ Chapter Zero.cpp
@@ -7,3 +7,16 @@ int main()
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
+
+
+
+// 0 - 2
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "This (\") is a quote , and this (\\) is a backlash." << endl;
+	return 0;
+}
