1c8d197538ca1345cb2e44da1af2d1e6d2388d2f
Misaka 9936 <Misaka9936@gmail.com>
2020-09-03 00:14:49 +0800

    Update 2. Chapter Two.cpp

--- 2. Chapter Two.cpp
+++ 2. Chapter Two.cpp
@@ -59,3 +59,428 @@ int main()
 
 	return 0;
 }
+
+
+
+// 2 - 1
+
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+	cout << "Please enter your first name : ";
+
+	string name;
+	cin >> name;
+
+	const string greeting = "Hello, " + name + "!";
+	//pad == 0时没有间隔
+	const int pad = 0;
+
+	const int rows = pad * 2 + 3;
+	const string::size_type cols = greeting.size() + pad * 2 + 2;
+
+	cout << endl;
+
+	for (int r = 0; r != rows; ++r) {
+
+		string::size_type c = 0;
+
+		while (c != cols) {
+
+			if (r == pad + 1 && c == pad + 1) {
+				cout << greeting;
+				c += greeting.size();
+			}
+			else {
+				if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1)
+					cout << "*";
+				else
+					cout << " ";
+				++c;
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+
+
+// 2 - 2.1
+//让空行变为上下各两行
+
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+	cout << "Please enter your first name : ";
+
+	string name;
+	cin >> name;
+
+	const string greeting = "Hello, " + name + "!";
+	const int pad = 1;
+	
+	//
+	const int rows = pad * 4 + 3;
+	const string::size_type cols = greeting.size() + pad * 2 + 2;
+
+	cout << endl;
+
+	for (int r = 0; r != rows; ++r) {
+
+		string::size_type c = 0;
+
+		while (c != cols) {
+			
+			//
+			if (r == pad + 2 && c == pad + 1) {
+				cout << greeting;
+				c += greeting.size();
+			}
+			else {
+				if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1)
+					cout << "*";
+				else
+					cout << " ";
+				++c;
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+
+
+// 2 - 2.2
+//让空列变为两列
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+	cout << "Please enter your first name : ";
+
+	string name;
+	cin >> name;
+
+	const string greeting = "Hello, " + name + "!";
+	const int pad = 1;
+
+	const int rows = pad * 2 + 3;
+	//
+	const string::size_type cols = greeting.size() + pad * 4 + 2;
+
+	cout << endl;
+
+	for (int r = 0; r != rows; ++r) {
+
+		string::size_type c = 0;
+
+		while (c != cols) {
+			//
+			if (r == pad + 1 && c == pad + 2) {
+				cout << greeting;
+				c += greeting.size();
+			}
+			else {
+				if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1)
+					cout << "*";
+				else
+					cout << " ";
+				++c;
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+
+
+// 2 - 3
+
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+	cout << "Please enter your first name : ";
+
+	string name;
+	cin >> name;
+
+	const string greeting = "Hello, " + name + "!";
+	//
+	int pad;
+	cout << "Please enter the custom pad (recommend 1) : ";
+	cin >> pad;
+	
+
+	const int rows = pad * 2 + 3;
+	const string::size_type cols = greeting.size() + pad * 2 + 2;
+
+	cout << endl;
+
+	for (int r = 0; r != rows; ++r) {
+
+		string::size_type c = 0;
+
+		while (c != cols) {
+
+			if (r == pad + 1 && c == pad + 1) {
+				cout << greeting;
+				c += greeting.size();
+			}
+			else {
+				if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1)
+					cout << "*";
+				else
+					cout << " ";
+				++c;
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+
+
+// 2 - 4
+
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+	cout << "Please enter your first name : ";
+
+	string name;
+	cin >> name;
+
+	const string greeting = "Hello, " + name + "!";
+	const int pad = 1;
+
+	const int rows = pad * 2 + 3;
+	const string::size_type cols = greeting.size() + pad * 2 + 2;
+	
+	//
+	const string spaces(greeting.size() + pad * 2, ' ');
+
+	cout << endl;
+
+	for (int r = 0; r != rows; ++r) {
+
+		string::size_type c = 0;
+
+		while (c != cols) {
+
+			if (r == pad + 1 && c == pad + 1) {
+				cout << greeting;
+				c += greeting.size();
+			}
+			else {
+				if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1) {
+					cout << "*";
+					//
+					++c;
+				}
+				//
+				else {
+					if (r == pad + 1) {
+						cout << ' ';
+						++c;
+					}
+					else {
+						cout << spaces;
+						c += spaces.size();
+					}
+				}
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+
+
+// 2 - 5.1
+//输出一个四周*数相同的正方形
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int length;
+	cout << "Please enter the length of square : ";
+	cin >> length;
+
+	cout << endl;
+
+	for (int i = 0; i != length; ++i) {
+		
+		int j = 0;
+		while (j != length) {
+
+			if (i == 0 || i == length - 1 || j == 0 || j == length - 1) {
+				cout << '*';
+			}
+			else {
+				cout << ' ';
+			}
+			++j;
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+
+
+
+// 2 - 5.2
+//这个正方形更像是正方形了
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int length;
+	cout << "Please enter the length of square : ";
+	cin >> length;
+
+	string starpace = "* ";
+	cout << endl;
+
+	for (int i = 0; i != length; ++i) {
+		
+		int j = 0;
+		while (j != length * 2) {
+
+			if (i == 0 || i == length - 1 || j == 0 || j == length * 2 - 2) {
+				cout << starpace;
+				j += 2;
+			}
+			else {
+				cout << ' ';
+				++j;
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+
+
+// 2 - 5.3
+//生成一个长方形
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int length, width;
+	cout << "Please enter the length of rectangle : ";
+	cin >> length;
+	cout << "Please enter the width of rectangle : ";
+	cin >> width;
+
+	cout << endl;
+
+	for (int i = 0; i != width; ++i) {
+
+		int j = 0;
+		while (j != length) {
+
+			if (i == 0 || i == width - 1 || j == 0 || j == length - 1) {
+				cout << '*';
+			}
+			else {
+				cout << ' ';
+			}
+			++j;
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+
+
+// 2 - 5.4
+//现在更像一个长方形了
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int length, width;
+	cout << "Please enter the length of rectangle : ";
+	cin >> length;
+	cout << "Please enter the width of rectangle : ";
+	cin >> width;
+
+	string starpace = "* ";
+
+	cout << endl;
+
+	for (int i = 0; i != width; ++i) {
+
+		int j = 0;
+		while (j != length * 2) {
+
+			if (i == 0 || i == width - 1 || j == 0 || j == length * 2 - 2) {
+				cout << starpace;
+				j += 2;
+			}
+			else {
+				cout << ' ';
+				++j;
+			}
+			
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+
+
